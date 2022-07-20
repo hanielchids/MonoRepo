85d420b5605c1f997fa91ebbc60aebbab2793341
Haniel Chidavose <Haniel.Chidavose@sovtech.com>
2022-07-20 14:45:50 +0200

    feat(login): user authentication created

--- Client/src/App.js
+++ Client/src/App.js
@@ -3,6 +3,8 @@ import { BrowserRouter as Router, Route, Switch, Link } from "react-router-dom";
 import Home from "./Pages/Home";
 import CreatePost from "./Pages/CreatePost";
 import Post from "./Pages/Post";
+import Login from "./Pages/Login";
+import Registration from "./Pages/Registration";
 
 function App() {
   return (
@@ -11,11 +13,15 @@ function App() {
         <div className="navbar">
           <Link to="/createPost"> Create a post</Link>
           <Link to="/"> Home Page</Link>
+          <Link to="/login"> Login</Link>
+          <Link to="/registration"> Registration</Link>
         </div>
         <Switch>
           <Route path="/" exact component={Home} />
           <Route path="/createPost" exact component={CreatePost} />
           <Route path="/post/:id" exact component={Post} />
+          <Route path="/login" exact component={Login} />
+          <Route path="/registration" exact component={Registration} />
         </Switch>
       </Router>
     </div>
